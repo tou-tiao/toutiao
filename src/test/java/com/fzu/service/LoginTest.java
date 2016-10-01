abec3c1a7fb11b9bb58828487b7d857d0dfccae6
C32182119 <952614103@qq.com>
2016-10-01 22:09:53 +0800

    log and test

--- src/test/java/com/fzu/service/LoginTest.java
+++ src/test/java/com/fzu/service/LoginTest.java
@@ -1,30 +1,34 @@
 package com.fzu.service;
 
-import com.fzu.Application;
 import com.fzu.pojo.User;
 import com.fzu.repository.UserRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
-@IntegrationTest
-@Transactional
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class LoginTest {
 
+    private Logger logger =  LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
     private UserRepository userRepository;
     @Test
-    private void InsertRecord(){
+    public void InsertRecord(){
         User user = new User();
-        user.setEmail("1034@qq.com");
-        userRepository.save(user);
+        user.setId(1L);
+        userRepository.getOne(1L);
+//        user.setEmail("1034@qq.com");
+//        user.setPassword("123");
+//        userRepository.save(user);
+
+
+        logger.error("success");
     }
 }
