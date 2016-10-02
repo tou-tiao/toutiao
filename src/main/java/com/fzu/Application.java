4ffe101d500c1fe1cf0d3f7648ce67c955c00b0e
C32182119 <952614103@qq.com>
2016-10-02 22:05:53 +0800

    重构

--- src/main/java/com/fzu/Application.java
+++ src/main/java/com/fzu/Application.java
@@ -1,6 +1,5 @@
 package com.fzu;
 
-import com.fzu.controller.IndexController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
