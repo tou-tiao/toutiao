5bd65448940be28533318f7b9ae4a4772e7337a6
C32182119 <952614103@qq.com>
2016-10-07 16:14:52 +0800

    重构

---
+++ src/main/java/com/fzu/repository/SubscribeRepository.java
@@ -0,0 +1,8 @@
+package com.fzu.repository;
+
+import com.fzu.pojo.Subscribe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+
+}
